ca9ce6d66ba8ad7964b5cb8fd619bc985e77f676
Janak <jaykapadiya8899@gmail.com>
2023-03-13 17:16:39 +0530

    initial commit

---
+++ src/main/java/com/jms/springboot/sfgJMS/model/Message.java
@@ -0,0 +1,19 @@
+package com.jms.springboot.sfgJMS.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.UUID;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Message implements Serializable {
+
+    static final long serialVersionID = 123213213312313L;
+    private UUID id;
+    private String message;
+}
---
+++ src/test/java/com/jms/springboot/sfgJMS/SfgJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jms.springboot.sfgJMS;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgJmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
